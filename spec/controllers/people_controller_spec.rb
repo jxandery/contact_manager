521a11ae189f64194ed2e3a6d81de3f57dd9c3f7
Jack Yeh <jack.yeh@gmail.com>
2015-08-21 16:42:39 -0600

    implement validation on person

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe PeopleController, type: :controller do
   let(:invalid_attributes) {
     {first_name: nil, last_name: nil}
   }
+
   let(:valid_session) { {} }
 
   describe "GET #index" do
@@ -76,14 +77,15 @@ RSpec.describe PeopleController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {first_name: "NewFirstName", last_name: "NewLastName"}
       }
 
       it "updates the requested person" do
         person = Person.create! valid_attributes
         put :update, {:id => person.to_param, :person => new_attributes}, valid_session
         person.reload
-        skip("Add assertions for updated state")
+        expect(person.first_name).to eq('NewFirstName')
+        expect(person.last_name).to eq('NewLastName')
       end
 
       it "assigns the requested person as @person" do
