e8672ef3a9558899b7777c47adb551cbd00fb6a9
Jack Yeh <jack.yeh@gmail.com>
2015-08-21 16:29:11 -0600

    delete extraneous file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426010626) do
+ActiveRecord::Schema.define(version: 20150426013337) do
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string   "number"
+    t.integer  "person_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
