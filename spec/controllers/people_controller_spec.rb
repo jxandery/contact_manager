dfb843dfdb5105beef7a86a74bc9fa95a1bc8ec1
Jack Yeh <jack.yeh@gmail.com>
2015-08-21 16:30:50 -0600

    add valid attributes to people controller spec

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 RSpec.describe PeopleController, type: :controller do
   let(:valid_attributes) {
+    {first_name: 'Jane', last_name: 'Doe'}
   }
 
   let(:invalid_attributes) {
