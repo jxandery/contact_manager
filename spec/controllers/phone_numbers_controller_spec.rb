a8ce00decd2410cfda16c623b8d4230a0fbefe47
Jack Yeh <jack.yeh@gmail.com>
2015-08-23 20:19:52 -0600

    create/pass test to validate phone number: - add validations on phone numbers: phone number and person id - add valid attributes to spec files - add invalid attributes to spec files

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 RSpec.describe PhoneNumbersController, type: :controller do
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {number: "My String", person_id: 1}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {number: nil, person_id: nil}
   }
 
   let(:valid_session) { {} }
@@ -77,14 +77,15 @@ RSpec.describe PhoneNumbersController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {number: "my string", person_id: 2}
       }
 
       it "updates the requested phone_number" do
         phone_number = PhoneNumber.create! valid_attributes
         put :update, {:id => phone_number.to_param, :phone_number => new_attributes}, valid_session
         phone_number.reload
-        skip("Add assertions for updated state")
+        expect(phone_number.number).to eq("my string")
+        expect(phone_number.person_id).to eq(2)
       end
 
       it "assigns the requested phone_number as @phone_number" do
