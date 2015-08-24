56c0fac636522b5a637a2bf5f8b12248f1c0995c
Jack Yeh <jack.yeh@gmail.com>
2015-08-23 20:09:55 -0600

    add validation: phone number is required - create test phone number is valid - create test phone number is not valid with out a number

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
+  let(:phone_number) {PhoneNumber.new(number: "112233445")}
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+
+  it 'is invalid without a phone number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
 end
