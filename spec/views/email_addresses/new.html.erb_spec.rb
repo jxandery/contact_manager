cd3e7e057cc2b89f9db3af6ec5d67da00d6ff060
Jack Yeh <jack.yeh@gmail.com>
2015-09-14 16:43:22 -0600

    create/pass test: renders new email address form with name in the title

--- spec/views/email_addresses/new.html.erb_spec.rb
+++ spec/views/email_addresses/new.html.erb_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "email_addresses/new", type: :view do
+  let(:person) {Person.create!(first_name: 'Bob', last_name: 'Smith')}
+  let(:email_address) {EmailAddress.new(contact_id: person.id, contact_type: 'Person', address: 'MyString')}
+
   before(:each) do
-    assign(:email_address, EmailAddress.new(
-      :address => "MyString",
-      :contact_id => 1
-    ))
+    assign(:email_address, email_address)
   end
+
 
   it "renders new email_address form" do
     render
@@ -17,5 +18,10 @@ RSpec.describe "email_addresses/new", type: :view do
 
       assert_select "input#email_address_contact_id[name=?]", "email_address[contact_id]"
     end
+  end
+
+  it 'shows the contacts name in the title' do
+    render
+    assert_select('h1', text: "New Email Address for #{email_address.contact.last_name}, #{email_address.contact.first_name}")
   end
 end
